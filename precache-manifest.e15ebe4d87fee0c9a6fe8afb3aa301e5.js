45d608a027e55e83b2e370e5a8bb2b0c0d6a654b
Chintan295 <chintansavaliya2000@gmail.com>
2020-03-20 15:25:41 +0530

    Updates

---
+++ precache-manifest.e15ebe4d87fee0c9a6fe8afb3aa301e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88aeff70c790febb4e53cb88c7d7745",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "810f53ccbf6a44f6d1ea",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9e8d3df11ee0f074062",
+    "url": "/my-app/static/js/2.a681efe0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.a681efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810f53ccbf6a44f6d1ea",
+    "url": "/my-app/static/js/main.a8bd5892.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
