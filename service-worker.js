a2be303286f25c0d07cb9b9ec909dd6c27cde98e
chintan295 <chintansavaliya2000@gmail.com>
2020-04-28 11:32:18 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.eabe8779050d0928738ce71d04dbab47.js"
+  "/my-app/precache-manifest.027039d9f64e1c7e66749424a63e6a25.js"
 );
 
 self.addEventListener('message', (event) => {
